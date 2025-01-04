600128c26ce87a9383f4b599f1bd0c8f727d703a
Purpkin <guiejo2020@gmail.com>
2025-01-04 11:46:05 -0300

    Pool stick hand and multiple red balls

--- BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBoss.cs
+++ BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBoss.cs
@@ -2,6 +2,7 @@ namespace Game.Bosses.Snooker
 {
     using System.Collections;
     using DG.Tweening;
+    using Game.Player;
     using UnityEngine;
     using UnityHFSM;
 
@@ -16,10 +17,16 @@ namespace Game.Bosses.Snooker
         public float shotAnticipationSecs = 2f;
         public float ballBouceTime = 5f;
         public float ballStopLinearDrag = 0.25f;
+        public float redBallStopLinearDrag = 0.1f;
+
+        [Header("Visual")]
+        public float poolHandDistance = 3f;
 
         [Header("References")]
         public Rigidbody2D whiteBall;
+        public Rigidbody2D[] redBalls;
         public Transform poolStick;
+        public Transform poolStickHand;
 
         private StateMachine fsm;
 
@@ -34,9 +41,14 @@ namespace Game.Bosses.Snooker
 
             Vector2 dir = player.position - whiteBall.transform.position;
             dir.Normalize();
-            poolStick.DOMove(whiteBall.position - dir * 1.5f, 1f);
+
+            poolStick.DOMove(whiteBall.position - dir * 1.6f, 1f);
             poolStick.DORotate(Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x) * Vector3.forward, 0.9f)
-            .SetEase(Ease.OutCubic);
+                .SetEase(Ease.OutCubic);
+            poolStickHand.DOMove(whiteBall.position - dir * (1.6f + poolHandDistance), 1f);
+            poolStickHand.DORotate(Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x) * Vector3.forward, 0.9f)
+                .SetEase(Ease.OutCubic);
+
             _nextStep += 0.9f;
             yield return new WaitWhile(() => state.timer.Elapsed < _nextStep);
 
@@ -47,8 +59,10 @@ namespace Game.Bosses.Snooker
                 dir = player.transform.position - whiteBall.transform.position;
                 dir.Normalize();
                 poolStick.right = dir;
+                poolStickHand.right = dir;
 
                 poolStick.transform.position = whiteBall.position - dir * 1.6f;
+                poolStickHand.transform.position = whiteBall.position - dir * (1.6f + poolHandDistance);
 
                 yield return null;
             }
@@ -61,26 +75,44 @@ namespace Game.Bosses.Snooker
             poolStick.DOMove(whiteBall.position - dir * 1.45f, 0.1f).SetEase(Ease.Linear);
             yield return new WaitWhile(() => state.timer.Elapsed < _nextStep);
 
+            whiteBall.linearDamping = 0;
+            foreach (var ball in redBalls)
+                ball.linearDamping = 0;
             whiteBall.AddForce(dir * shotForce, ForceMode2D.Impulse);
+
             poolStick.DORotate(Vector3.forward * -90, 1.5f).SetEase(Ease.OutSine);
             poolStick.DOMove(transform.position + Vector3.right, 1.5f).SetEase(Ease.OutSine);
+
+            poolStickHand.DOMove(transform.position, 1.5f).SetEase(Ease.OutSine);
+            poolStickHand.DORotate(Vector3.forward * -90, 1.5f).SetEase(Ease.OutSine);
 
 
             _nextStep += ballBouceTime;
             yield return new WaitWhile(() => state.timer.Elapsed < _nextStep);
 
-            whiteBall.linearDamping = ballStopLinearDrag;
             _nextStep += 2f;
-            yield return new WaitWhile(() => state.timer.Elapsed < _nextStep);
+            while (state.timer.Elapsed < _nextStep)
+            {
+                whiteBall.linearDamping += ballStopLinearDrag * Time.fixedDeltaTime;
+                foreach (var ball in redBalls)
+                    ball.linearDamping += redBallStopLinearDrag * Time.fixedDeltaTime;
+                yield return new WaitForFixedUpdate();
+            }
 
-            whiteBall.linearDamping = 0;
+            // whiteBall.linearDamping = 30;
             fsm.StateCanExit();
         }
 
         private void Start()
         {
             fsm = new StateMachine();
-            fsm.AddState(IdleCooldownState, onEnter: _ => whiteBall.linearVelocity = Vector2.zero);
+            fsm.AddState(IdleCooldownState, onEnter: _ =>
+            {
+                CameraManager.Instance.FocusUp();
+            }, onExit: _ =>
+            {
+                CameraManager.Instance.ResetFocus();
+            });
 
             fsm.AddState(ShotWhiteBallState, new CoState(
                 this,
---
+++ BossRushGame/Assets/Scripts/Player/CameraManager.cs
@@ -0,0 +1,37 @@
+namespace Game.Player
+{
+    using DG.Tweening;
+    using Pixelplacement;
+    using Unity.Cinemachine;
+    using UnityEngine;
+
+    public class CameraManager : Singleton<CameraManager>
+    {
+        public CinemachinePositionComposer positionComposer;
+
+        public float focusUpScreenY = 0.1f;
+        public float focusDuration = 1f;
+
+        public void FocusUp()
+        {
+            this.DOKill();
+            DOTween.To(
+                () => positionComposer.Composition.ScreenPosition.y,
+                f => positionComposer.Composition.ScreenPosition = new Vector2(positionComposer.Composition.ScreenPosition.x, f),
+                focusUpScreenY,
+                focusDuration
+            ).SetTarget(this);
+        }
+
+        public void ResetFocus()
+        {
+            this.DOKill();
+            DOTween.To(
+                () => positionComposer.Composition.ScreenPosition.y,
+                f => positionComposer.Composition.ScreenPosition = new Vector2(positionComposer.Composition.ScreenPosition.x, f),
+                0,
+                focusDuration
+            ).SetTarget(this);
+        }
+    }
+}
