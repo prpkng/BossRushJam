0af1830af9cb24a355892f9c2e283b447402244c
prpkng <guiejo2020@gmail.com>
2025-01-06 22:54:12 -0300

    Fixed exceptions when destroying balls during attack phase

--- BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBoss.cs
+++ BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBoss.cs
@@ -1,6 +1,7 @@
 using System.Linq;
 using Game.Systems;
 using PrimeTween;
+using UnityEngine.Serialization;
 
 namespace Game.Bosses.Snooker
 {
@@ -11,79 +12,130 @@ namespace Game.Bosses.Snooker
 
     public class SnookerBoss : MonoBehaviour
     {
-        [Header("Main Idle")]
-        public float idleWaitTimeMin = 4;
+        [Header("Main Idle")] public float idleWaitTimeMin = 4;
         public float idleWaitTimeMax = 6;
 
-        [Header("Shot White Ball")]
-        public float shotBallWaitTime = 5f;
+        [Header("Shot White Ball")] public float shotBallWaitTime = 5f;
 
         public float whiteBallStopThreshold = 1f;
-        [Space]
-        public float shotForce = 25;
+        [Space] public float shotForce = 25;
         public float shotAnticipationSecs = 2f;
-        public float ballStopLinearDrag = 0.25f;
-        public float redBallStopLinearDrag = 0.1f;
+        [FormerlySerializedAs("ballStopLinearDrag")] public float currentBallStopLinearDrag = 0.25f;
+        [FormerlySerializedAs("redBallStopLinearDrag")] public float otherBallsStopLinearDrag = 0.1f;
 
-        [Header("Visual")]
-        public float poolHandDistance = 3f;
-        
-        [Header("References")]
-        public Rigidbody2D[] availableBalls;
+        [Header("Visual")] public float poolHandDistance = 3f;
+
+        [Header("References")] public Rigidbody2D[] availableBalls;
         public Transform poolStick;
         public Transform poolStickHand;
 
         private StateMachine fsm;
+        private Rigidbody2D _currentBall;
 
+        // State names
         private const string IdleCooldownState = "IdleCooldown";
         private const string AttackCooldownState = "AttackCooldown";
-        private const string ShotWhiteBallState = "ShotWhiteBall";
-
-        private IEnumerator ShotWhiteBall(CoState<string, string> state)
+        private const string ShotBallState = "ShotBall";
+        private const string SearchForBallsState = "SearchForBalls";
+        private void Start()
         {
-            availableBalls = availableBalls.Where(b => b != null).ToArray();
-            var currentBall = availableBalls.ChooseRandom();
+            fsm = new StateMachine();
+            
+            // IDLE COOLDOWN STATE
+            fsm.AddState(
+                IdleCooldownState, 
+                onEnter: _ => { CameraManager.Instance.FocusUp(); },
+                onExit: _ => { CameraManager.Instance.ResetFocus(); }
+            );
+            fsm.AddTransition(
+                new TransitionAfterDynamic(
+                    IdleCooldownState,
+                    SearchForBallsState,
+                    s => Random.Range(idleWaitTimeMin, idleWaitTimeMax)
+                )
+            );
+
+            // SEARCH FOR BALLS STATE
+            fsm.AddState(SearchForBallsState, onEnter: _ =>
+            {
+                availableBalls = availableBalls.Where(b => b != null).ToArray();
+                _currentBall = availableBalls.ChooseRandom();
+                print($"Chosen a random ball of all available: {_currentBall}");
+            }, isGhostState: true);
+            fsm.AddTransition(SearchForBallsState, ShotBallState);
+            
+            // SHOT BALL STATE
+            fsm.AddState(
+                ShotBallState,
+                new ParallelStates(
+                    needsExitTime: true,
+                    
+                    new CoState(
+                        this,
+                        ShotWhiteBallCoroutine,
+                        needsExitTime: true,
+                        loop: false
+                    ),
+                    new State(onLogic: state =>
+                    {
+                        if (_currentBall) return;
+                        print("The main ball was destroyed, trying again");
+                        fsm.RequestStateChange(SearchForBallsState, true);
+                    })
+                )
+            );
+            fsm.AddTransition(ShotBallState, IdleCooldownState);
+
+            fsm.Init();
+        }
+        
+        
+        private IEnumerator ShotWhiteBallCoroutine(CoState<string, string> state)
+        {
             float nextStep = 0;
             var player = GameManager.Instance.Player.transform;
 
-            Vector2 dir = player.position - currentBall.transform.position;
+            Vector2 dir = player.position - _currentBall.transform.position;
             dir.Normalize();
 
-            Tween.Position(poolStick, currentBall.position - dir * 1.6f, 1, Ease.OutCubic);
+            // Move stick to ball
+            Tween.Position(poolStick, _currentBall.position - dir * 1.6f, 1, Ease.OutCubic);
             Tween.Rotation(poolStick, Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x) * Vector3.forward, 0.9f, Ease.OutCubic);
-            
-            Tween.Position(poolStickHand, currentBall.position - dir * (1.6f + poolHandDistance), 1f);
-            Tween.Rotation(poolStickHand, Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x) * Vector3.forward, 0.9f, Ease.OutCubic);
+
+            // Move pool hand too
+            Tween.Position(poolStickHand, _currentBall.position - dir * (1.6f + poolHandDistance), 1f);
+            Tween.Rotation(poolStickHand, Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x) * Vector3.forward, 0.9f,
+                Ease.OutCubic);
 
             nextStep += 0.9f;
             yield return new WaitWhile(() => state.timer.Elapsed < nextStep);
-
-
+            
             nextStep += shotAnticipationSecs;
             while (state.timer.Elapsed < nextStep)
             {
-                dir = player.transform.position - currentBall.transform.position;
+                dir = player.transform.position - _currentBall.transform.position;
                 dir.Normalize();
                 poolStick.right = dir;
                 poolStickHand.right = dir;
 
-                poolStick.transform.position = currentBall.position - dir * 1.6f;
-                poolStickHand.transform.position = currentBall.position - dir * (1.6f + poolHandDistance);
+                poolStick.transform.position = _currentBall.position - dir * 1.6f;
+                poolStickHand.transform.position = _currentBall.position - dir * (1.6f + poolHandDistance);
 
                 yield return null;
             }
 
             nextStep += 0.55f;
-            Tween.Position(poolStick, currentBall.position - dir * 3, 0.5f, Ease.OutCubic);
+            Tween.Position(poolStick, _currentBall.position - dir * 3, 0.5f, Ease.OutCubic);
             yield return new WaitWhile(() => state.timer.Elapsed < nextStep);
 
             nextStep += 0.1f;
-            Tween.Position(poolStick, currentBall.position - dir * 1.45f, 0.1f, Ease.Linear);
+            Tween.Position(poolStick, _currentBall.position - dir * 1.45f, 0.1f, Ease.Linear);
             yield return new WaitWhile(() => state.timer.Elapsed < nextStep);
 
             foreach (var ball in availableBalls)
-                ball.linearDamping = 0;
-            currentBall.linearVelocity = dir * shotForce;
+                if (ball) ball.linearDamping = 0;
+            
+            _currentBall.linearVelocity = dir * shotForce;
 
             Tween.Rotation(poolStick, Vector3.forward * -90, 1.5f, Ease.OutSine);
             Tween.Position(poolStick, transform.position + Vector3.right, 1.5f, Ease.OutSine);
@@ -93,51 +145,21 @@ namespace Game.Bosses.Snooker
 
 
             nextStep += shotBallWaitTime;
-            yield return new WaitWhile(() => state.timer.Elapsed < nextStep && currentBall.linearVelocity.magnitude > whiteBallStopThreshold);
+            yield return new WaitWhile(() =>
+                state.timer.Elapsed < nextStep && _currentBall.linearVelocity.magnitude > whiteBallStopThreshold);
 
             nextStep += 2f;
-            
-            var otherBalls = availableBalls.Where(b => b != currentBall);
+
+            var otherBalls = availableBalls.Where(b => b != _currentBall).ToArray();
             while (state.timer.Elapsed < nextStep)
             {
-                currentBall.linearDamping += ballStopLinearDrag * Time.fixedDeltaTime;
+                _currentBall.linearDamping += currentBallStopLinearDrag * Time.fixedDeltaTime;
                 foreach (var ball in otherBalls)
-                    ball.linearDamping += redBallStopLinearDrag * Time.fixedDeltaTime;
+                    if (ball) ball.linearDamping += otherBallsStopLinearDrag * Time.fixedDeltaTime;
                 yield return new WaitForFixedUpdate();
             }
 
             fsm.StateCanExit();
-        }
-
-        private void Start()
-        {
-            fsm = new StateMachine();
-            fsm.AddState(IdleCooldownState, onEnter: _ =>
-            {
-                CameraManager.Instance.FocusUp();
-            }, onExit: _ =>
-            {
-                CameraManager.Instance.ResetFocus();
-            });
-
-            fsm.AddState(ShotWhiteBallState, new CoState(
-                this,
-                ShotWhiteBall,
-                needsExitTime: true,
-                loop: false
-            ));
-
-            fsm.AddTransition(
-                new TransitionAfterDynamic(
-                    IdleCooldownState,
-                    ShotWhiteBallState,
-                    s => Random.Range(idleWaitTimeMin, idleWaitTimeMax)
-                )
-            );
-
-            fsm.AddTransition(ShotWhiteBallState, IdleCooldownState);
-
-            fsm.Init();
         }
 
         private void FixedUpdate()
---
+++ BossRushGame/Assets/Scripts/Bosses/Snooker/States/ShotWhiteBall.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityHFSM;
+
+namespace Game.Bosses.Snooker.States
+{
+    // public class ShotWhiteBall : CoState
+    // {
+    //     
+    // }
+}
