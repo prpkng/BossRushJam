db11c673450ede176373d2436b9d06e906b32b1c
prpkng <guiejo2020@gmail.com>
2025-01-06 23:30:05 -0300

    Fixed ball colors

--- BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBall.cs
+++ BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBall.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Game.Systems;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -6,17 +9,29 @@ namespace Game.Bosses.Snooker
 {
     public class SnookerBall : MonoBehaviour
     {
+        private static readonly List<Color> PickedColors = new();
+        
         [SerializeField] private float damageSpeedThreshold = 5f;
         [SerializeField] private int hazardousLayer;
         [SerializeField] private int safeLayer;
         [SerializeField] private SpriteRenderer ballSprite;
-        
+        [SerializeField] private Color[] possibleColors;
         private Rigidbody2D _rb;
         
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
-            ballSprite.color = Random.ColorHSV(0, 1, 1, 1, 1, 1, 1, 1);
+            ballSprite.color = possibleColors.Except(PickedColors).ToArray().ChooseRandom();
+        }
+
+        private void OnEnable()
+        {
+            PickedColors.Add(ballSprite.color);
+        }
+
+        private void OnDisable()
+        {
+            PickedColors.Remove(ballSprite.color);
         }
 
         private void FixedUpdate()
