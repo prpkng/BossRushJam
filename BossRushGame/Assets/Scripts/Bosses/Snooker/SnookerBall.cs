3ef4929cf6999c1e613c7916526138143742c291
prpkng <guiejo2020@gmail.com>
2025-01-08 22:52:46 -0300

    Balls sprite and animation

--- BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBall.cs
+++ BossRushGame/Assets/Scripts/Bosses/Snooker/SnookerBall.cs
@@ -1,41 +1,49 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Game.Systems;
+using UnityEditor.Animations;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 namespace Game.Bosses.Snooker
 {
     public class SnookerBall : MonoBehaviour
     {
-        private static readonly List<Color> PickedColors = new();
+        private static readonly List<float> PickedHues = new();
         
         [SerializeField] private float damageSpeedThreshold = 5f;
         [SerializeField] private int hazardousLayer;
         [SerializeField] private int safeLayer;
         [SerializeField] private SpriteRenderer ballSprite;
-        [SerializeField] private Color[] possibleColors;
+        [SerializeField] private Animator ballAnimator;
+        [SerializeField] private AnimatorController[] possibleBallAnimations;
+        [SerializeField] private float[] possibleHues;
+        [SerializeField] private float ballSpeedMulti = 0.25f;
+        [SerializeField] private float ballSpeedPow = 1.1f;
         private Rigidbody2D _rb;
+
+        private float currentHue;
         
         private void Awake()
         {
             _rb = GetComponent<Rigidbody2D>();
-            ballSprite.color = possibleColors.Except(PickedColors).ToArray().ChooseRandom();
+            currentHue = possibleHues.Except(PickedHues).ToArray().ChooseRandom();
+            ballSprite.material.SetFloat("_Shift", currentHue);
+            ballAnimator.runtimeAnimatorController = possibleBallAnimations.ChooseRandom();
         }
 
         private void OnEnable()
         {
-            PickedColors.Add(ballSprite.color);
+            PickedHues.Add(currentHue);
         }
 
         private void OnDisable()
         {
-            PickedColors.Remove(ballSprite.color);
+            PickedHues.Remove(currentHue);
         }
 
         private void FixedUpdate()
         {
+            ballAnimator.speed = Mathf.Pow(_rb.linearVelocity.magnitude * ballSpeedMulti, ballSpeedPow);
             transform.right = _rb.linearVelocity.normalized;
 
             gameObject.layer = _rb.linearVelocity.magnitude > damageSpeedThreshold ? hazardousLayer : safeLayer;
