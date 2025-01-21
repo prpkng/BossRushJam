9001e7e4ae4b89b0406d7f5d8cc74c59503f9d4e
prpkng <guiejo2020@gmail.com>
2025-01-20 22:34:46 -0300

    Removed pixel perfect hud and Give Up button

--- BossRushGame/Assets/Scripts/UI/PauseManager.cs
+++ BossRushGame/Assets/Scripts/UI/PauseManager.cs
@@ -1,13 +1,23 @@
 namespace BRJ.UI {
+    using Pixelplacement;
     using UnityEngine;
     using UnityEngine.EventSystems;
 
     public class PauseManager : MonoBehaviour {
-
         public GameObject resumeButton;
 
         public void ResetPause() {
             EventSystem.current.SetSelectedGameObject(resumeButton);
+        }
+
+
+        public void OpenSettings() {
+
+        }
+
+        public void GiveUp() {
+            Game.Instance.SetPaused(false);
+            Game.Instance.World.PlayerDeath();
         }
     }
 }
