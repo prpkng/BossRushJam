d23a84f31a9a05311af149932d0a5a52f79735f6
Purpkin <guiejo2020@gmail.com>
2025-01-31 19:47:06 -0300

    Fixed main menu hover and volume slider with mouse

--- BossRushGame/Assets/Scripts/UI/MainMenu/VolumeControl.cs
+++ BossRushGame/Assets/Scripts/UI/MainMenu/VolumeControl.cs
@@ -2,8 +2,10 @@ namespace BRJ.UI
 {
     using UnityEngine;
     using UnityEngine.EventSystems;
+    using UnityEngine.InputSystem;
+    using UnityEngine.UI;
 
-    public class VolumeControl : MonoBehaviour, IMoveHandler
+    public class VolumeControl : MonoBehaviour, IMoveHandler, IPointerEnterHandler, IPointerExitHandler
     {
         public float value = 1f;
         public RectTransform handle;
@@ -12,10 +14,13 @@ namespace BRJ.UI
         public string vcaPath = "vca:/";
         public float increment = 0.05f;
 
+        private bool isMouseInside;
         private FMOD.Studio.VCA vca;
+        private CanvasScaler canvas;
 
         private void Awake()
         {
+            canvas = GetComponentInParent<CanvasScaler>();
             vca = FMODUnity.RuntimeManager.GetVCA(vcaPath);
             if (PlayerPrefs.HasKey(key))
                 value = PlayerPrefs.GetFloat(key);
@@ -46,6 +51,46 @@ namespace BRJ.UI
                     UpdatePosition();
                     break;
             }
+        }
+
+        private bool isDragging;
+
+        private void Update()
+        {
+            if (InputManager.isUsingGamepad) return;
+            if (!isMouseInside && !isDragging) return;
+            if (!Mouse.current.leftButton.isPressed)
+            {
+                isDragging = false;
+                return;
+            }
+
+            isDragging = true;
+
+            var viewport = Mouse.current.position.ReadValue() / new Vector2(Screen.width, Screen.height);
+
+            var mp = canvas.referenceResolution * viewport;
+            var pos = (Vector2)transform.position - canvas.referenceResolution / 2f;
+            print(mp.x - pos.x);
+
+            var lastValue = value;
+            value = (mp.x - pos.x) / width;
+            if (value != lastValue)
+                UpdatePosition();
+
+            print(value);
+
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isMouseInside = false;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isMouseInside = true;
+            print("MouseEntered");
         }
     }
 }
