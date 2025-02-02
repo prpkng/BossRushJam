efafd24f7e9c60fc45e399458b79c7953afb14c8
Purpkin <guiejo2020@gmail.com>
2025-02-02 13:34:46 -0300

    Fixed volume sliders

--- BossRushGame/Assets/Scripts/UI/MainMenu/VolumeControl.cs
+++ BossRushGame/Assets/Scripts/UI/MainMenu/VolumeControl.cs
@@ -5,7 +5,7 @@ namespace BRJ.UI
     using UnityEngine.InputSystem;
     using UnityEngine.UI;
 
-    public class VolumeControl : MonoBehaviour, IMoveHandler, IPointerEnterHandler, IPointerExitHandler
+    public class VolumeControl : MonoBehaviour, IMoveHandler
     {
         public float value = 1f;
         public RectTransform handle;
@@ -18,6 +18,8 @@ namespace BRJ.UI
         private FMOD.Studio.VCA vca;
         private CanvasScaler canvas;
         private Camera cam;
+
+        private Vector2 pointerPos;
 
         private void Awake()
         {
@@ -38,75 +40,29 @@ namespace BRJ.UI
             PlayerPrefs.Save();
         }
 
+        public void MoveLeft()
+        {
+            value -= increment;
+            UpdatePosition();
+        }
+        public void MoveRight()
+        {
+            value += increment;
+            UpdatePosition();
+        }
         public void OnMove(AxisEventData eventData)
         {
             switch (eventData.moveDir)
             {
                 case MoveDirection.Left:
                     eventData.Use();
-                    value -= increment;
-                    UpdatePosition();
+                    MoveLeft();
                     break;
                 case MoveDirection.Right:
                     eventData.Use();
-                    value += increment;
-                    UpdatePosition();
+                    MoveRight();
                     break;
             }
-        }
-
-        private bool isDragging;
-
-        private void Update()
-        {
-            print($"{Screen.width}x{Screen.height}");
-            if (InputManager.isUsingGamepad) return;
-            if (!isMouseInside && !isDragging) return;
-            if (!Mouse.current.leftButton.isPressed)
-            {
-                isDragging = false;
-                return;
-            }
-
-            isDragging = true;
-
-            float w = Screen.width;
-            float h = Screen.height;
-            if (w / 16f > h / 9f)
-                w = h / 9f * 16f;
-            else
-                h = w / 16f * 9f;
-
-            Vector2 viewportPos = Mouse.current.position.ReadValue() /
-                new Vector2(w, h);
-
-            RectTransform rect = (RectTransform)canvas.transform;
-            var mp = rect.rect.size * viewportPos;
-            print(mp);
-
-            var pos = (Vector2)transform.position - canvas.referenceResolution / 2f;
-            mp.x -= width;
-            print("this pos: " + pos.x);
-            print(mp.x - pos.x);
-
-            var lastValue = value;
-            value = (mp.x - pos.x) / width;
-            if (value != lastValue)
-                UpdatePosition();
-
-            print(value);
-
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            isMouseInside = false;
-        }
-
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            isMouseInside = true;
-            print("MouseEntered");
         }
     }
 }
