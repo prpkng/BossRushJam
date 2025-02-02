b58f797a5e2821c9ddd5795a41d32b697f7ff695
prpkng <68878717+prpkng@users.noreply.github.com>
2025-02-02 18:58:13 +0000

    Deploying to gh-pages from @ prpkng/BossRushJam@4127578e1b135528b9c59d924effd04fb221938b 🚀

--- BossRushGame/ServiceWorker.js
+++ BossRushGame/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = "Casiverse-Casiland-0.0.33";
+const cacheName = "Casiverse-Casiland-0.0.34";
 const contentToCache = [
     "Build/BossRushGame.loader.js",
     "Build/BossRushGame.framework.js",
